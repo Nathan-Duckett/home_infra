2323accbf80d2690154d41a95ba296844c1ff710
Nathan <nduckett13@gmail.com>
2022-07-19 22:23:52 +1200

    Update traefik to use DNS resolution via Cloudflare

---
+++ help.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setting all objects within this file to be exported by default
+# https://unix.stackexchange.com/a/430690
+set -a
+
+HELP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+function prepare_config_file() {
+    CONFIG_FILE_NAME="$1"
+    env $(grep -v "^#" "$HELP_DIR/.env" | xargs -d "\n") envsubst < "$HELP_DIR/config/$CONFIG_FILE_NAME"
+}
