acd9938e4025f362206ebdf8665e16a919a38ea1
Nathan <nduckett13@gmail.com>
2022-07-19 22:27:29 +1200

    Add output to local_only for compiled configs

--- help.sh
+++ help.sh
@@ -8,5 +8,5 @@ HELP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function prepare_config_file() {
     CONFIG_FILE_NAME="$1"
-    env $(grep -v "^#" "$HELP_DIR/.env" | xargs -d "\n") envsubst < "$HELP_DIR/config/$CONFIG_FILE_NAME"
+    env $(grep -v "^#" "$HELP_DIR/.env" | xargs -d "\n") envsubst < "$HELP_DIR/config/$CONFIG_FILE_NAME" > "$HELP_DIR/local_only/$CONFIG_FILE_NAME"
 }
